98776709c3bf9a8a221beb126a0f3ec2a5d3b925
Thiago Santos <thiagosantosdev@gmail.com>
2021-07-08 16:26:24 -0300

    button login created

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { makeStyles, AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
+  },
+  appBar: {
+    boxShadow: 'none',
+  },
+  grow: {
+    flexGrow: 1,
   }
 }));
 
@@ -11,15 +19,13 @@ function Home() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar>
+      <AppBar color="inherit" className={classes.appBar}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <div className={classes.grow}/>
+          <Button startIcon={<AccountCircle />} variant="outlined" color="secondary">Fazer Login</Button>
         </Toolbar>
       </AppBar>
     </div>
