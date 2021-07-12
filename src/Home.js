28680f0a054e60030ed7be0a4a594b13079ed363
Thiago Santos <thiagosantosdev@gmail.com>
2021-07-12 15:54:55 -0300

    project done

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { makeStyles, AppBar, Toolbar, IconButton, Button, Drawer, List, ListItem, Divider, ListItemIcon, ListItemText, ListSubheader, Box, Typography } from '@material-ui/core';
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Button,
+  List,
+  Typography,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Box,
+  Grid,
+  Hidden,
+  Switch,
+} from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle'
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-import VideoCall from '@material-ui/icons/VideoCall'
-import MoreVert from '@material-ui/icons/MoreVert'
-import Apps from '@material-ui/icons/Apps'
+import Apps from '@material-ui/icons/Apps';
+import MoreVert from '@material-ui/icons/MoreVert';
+import VideoCall from '@material-ui/icons/VideoCall';
 
 import HomeIcon from '@material-ui/icons/Home';
 import Subscriptions from '@material-ui/icons/Subscriptions';
@@ -16,33 +35,32 @@ import History from '@material-ui/icons/History';
 
 import AddCircle from '@material-ui/icons/AddCircle';
 
-const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
+    backgroundColor: theme.palette.background.dark,
   },
   appBar: {
     boxShadow: 'none',
     zIndex: theme.zIndex.drawer + 1,
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    borderRight: 'none'
-  },
   logo: {
     height: 25,
   },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: 'none',
+  },
   menuIcon: {
     paddingRight: theme.spacing(5),
-    paddingLeft: theme.spacing(6)
+    paddingLeft: theme.spacing(6),
   },
   icons: {
-    paddingRight: theme.spacing(5)
+    paddingRight: theme.spacing(5),
   },
   grow: {
     flexGrow: 1,
@@ -56,63 +74,181 @@ const useStyles = makeStyles((theme) => ({
   },
   subheader: {
     textTransform: 'uppercase',
-  }
+  },
 }));
 
-function Home() {
+const videos = [
+  {
+    id: 1,
+    title:
+      'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+    channel: 'Lucas Nhimi',
+    views: '11 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb1.png',
+  },
+  {
+    id: 2,
+    title:
+      'COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02',
+    channel: 'Lucas Nhimi',
+    views: '957 mil visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb2.png',
+  },
+  {
+    id: 3,
+    title:
+      'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+    channel: 'Lucas Nhimi',
+    views: '106 mil visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb3.png',
+  },
+  {
+    id: 4,
+    title:
+      'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+    channel: 'Lucas Nhimi',
+    views: '5,6 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb4.png',
+  },
+  {
+    id: 5,
+    title:
+      'EDITOR DE POST COM MARKDOWN 2 | Criando uma Rede Social com React.js e .NET Core #26',
+    channel: 'Lucas Nhimi',
+    views: '2,2 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb5.png',
+  },
+  {
+    id: 6,
+    title: 'COMO MIGRAR PARA REACT HOOKS | Dicas e Truques #01',
+    channel: 'Lucas Nhimi',
+    views: '233 mil visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb6.png',
+  },
+  {
+    id: 7,
+    title:
+      'PRÉ-REQUISITOS | Criando uma Rede Social com React.js e .NET Core #01',
+    channel: 'Lucas Nhimi',
+    views: '118 mil visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb7.png',
+  },
+  {
+    id: 8,
+    title:
+      'GIT E GITHUB | Criando uma Rede Social com React.js e .NET Core #04',
+    channel: 'Lucas Nhimi',
+    views: '1,9 mi de visualizações',
+    date: 'há 1 semana',
+    avatar: '/images/avatar.jpeg',
+    thumb: '/images/thumb8.png',
+  },
+];
+
+function Home({ darkMode, setDarkMode }) {
   const classes = useStyles();
+  const theme = useTheme();
+
   return (
     <div className={classes.root}>
-      <AppBar color="inherit" className={classes.appBar}>
+      <AppBar color='inherit' className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuIcon} color="inherit" aria-label="menu">
+          <IconButton
+            edge='start'
+            className={classes.menuIcon}
+            aria-label='menu'
+          >
             <MenuIcon />
           </IconButton>
-          <img src="/images/preto.png" alt="logo" className={classes.logo}/>
-          <div className={classes.grow}/>
-          <IconButton className={classes.icons} color="inherit">
+          <img
+            src={
+              theme.palette.type === 'dark'
+                ? '/images/branco.png'
+                : '/images/preto.png'
+            }
+            alt='logo'
+            className={classes.logo}
+          />
+          <div className={classes.grow} />
+          <Switch
+            value={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+            className={classes.icons}
+          />
+          <IconButton className={classes.icons}>
             <VideoCall />
           </IconButton>
-          <IconButton className={classes.icons} color="inherit">
+          <IconButton className={classes.icons}>
             <Apps />
           </IconButton>
-          <IconButton className={classes.icons} color="inherit">
+          <IconButton className={classes.icons}>
             <MoreVert />
           </IconButton>
-          <Button startIcon={<AccountCircle />} variant="outlined" color="secondary">Fazer Login</Button>
+          <Button
+            startIcon={<AccountCircle />}
+            variant='outlined'
+            color='secondary'
+          >
+            Fazer Login
+          </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-              <ListItem button classes={{root: classes.listItem}}>
-                <ListItemIcon>{<HomeIcon/>}</ListItemIcon>
-                <ListItemText classes={{
-                  primary: classes.listItemText
-                }}primary={'Início'} />
-              </ListItem>
-              <ListItem button classes={{root: classes.listItem}}>
-              <ListItemIcon>{<Whatshot/>}</ListItemIcon>
-              <ListItemText classes={{
-                primary: classes.listItemText
-              }}primary={'Em Alta'} />
-            </ListItem>
-            <ListItem button classes={{root: classes.listItem}}>
-              <ListItemIcon>{<Subscriptions/>}</ListItemIcon>
-              <ListItemText classes={{
-                primary: classes.listItemText
-              }}primary={'Incrições'} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
+      <Box display='flex'>
+        <Hidden mdDown>
+          <Drawer
+            className={classes.drawer}
+            variant='permanent'
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<HomeIcon />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Início'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<Whatshot />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Em alta'}
+                  />
+                </ListItem>
+                <ListItem button classes={{ root: classes.listItem }}>
+                  <ListItemIcon>{<Subscriptions />}</ListItemIcon>
+                  <ListItemText
+                    classes={{
+                      primary: classes.listItemText,
+                    }}
+                    primary={'Inscrições'}
+                  />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
                 <ListItem button classes={{ root: classes.listItem }}>
                   <ListItemIcon>
                     <VideoLibrary />
@@ -135,9 +271,9 @@ function Home() {
                     primary={'Histórico'}
                   />
                 </ListItem>
-          </List>
-          <Divider />
-          <Box p={7}>
+              </List>
+              <Divider />
+              <Box p={7}>
                 <Typography variant='body2'>
                   Faça login para curtur vídeos, comentar e se inscrever.
                 </Typography>
@@ -267,11 +403,57 @@ function Home() {
                 />
               </ListItem>
               <Divider />
-        </div>
-      </Drawer>
-    </div>
-  )
-}
+            </div>
+          </Drawer>
+        </Hidden>
 
+        <Box p={8}>
+          <Toolbar />
+          <Typography
+            variant='h5'
+            color='textPrimary'
+            style={{ fontWeight: 600 }}
+          >
+            Recomendados
+          </Typography>
+
+          <Grid container spacing={4}>
+            {videos.map((item, index) => (
+              <Grid item lg={3} md={4} sm={6} xs={12}>
+                <Box>
+                  <img
+                    style={{ width: '100%' }}
+                    alt={item.title}
+                    src={item.thumb}
+                  />
+                  <Box>
+                    <Typography
+                      style={{ fontWeight: 600 }}
+                      gutterBottom
+                      variant='body1'
+                      color='textPrimary'
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      display='block'
+                      variant='body2'
+                      color='textSecondary'
+                    >
+                      {item.channel}
+                    </Typography>
+                    <Typography variant='body2' color='textSecondary'>
+                      {`${item.views} • ${item.date}`}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
+    </div>
+  );
+}
 
 export default Home;
